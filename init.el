702b2e19312ea2192cb2794f0de695a5d3748f50
chouaibdoct <alihaimoudchouaib@gmail.com>
2022-04-15 20:36:52 +0100

    exwm episode 1

--- init.el
+++ init.el
@@ -7,10 +7,10 @@
 
 
 (setq user-full-name "anaya")
-(setq gc-cons-threshold 100000000)
-;;(setq gc-cons-threshold 60000000)
+;;(setq gc-cons-threshold 100000000)
+(setq gc-cons-threshold 60000000)
 (setq comp-deferred-compilation t)
-
+;;(set-face-attribute 'default (selected-frame) :height 105)
 ;;;;;;;;;;
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -22,13 +22,29 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+(add-to-list 'load-path "~/.emacs.d/lisp")
 ;; (set-frame-parameter (selected-frame) 'alpha '(100 50))   ;; press C-x C-e if you want to set your screen transparent by 80%
 
 ;;;;;;;;;; bean dean config
+  
+
+;; (defvar bootstrap-version)
+;; (let ((bootstrap-file
+;;        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+;;       (bootstrap-version 5))
+;;   (unless (file-exists-p bootstrap-file)
+;;     (with-current-buffer
+;;         (url-retrieve-synchronously
+;;          "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+;;          'silent 'inhibit-cookies)
+;;       (goto-char (point-max))
+;;       (eval-print-last-sexp)))
+;;   (load bootstrap-file nil 'nomessage))
 
 
-
-
+(when (fboundp 'native-compile-async)
+  (setq comp-deferred-compilation t
+        comp-deferred-compilation-black-list '("/mu4e.*\\.el$")))
 
 
 
@@ -39,7 +55,7 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 
-
+;(native-compile-async "~/.config/emacs/elpa/" 6 t)
 
 
 
@@ -87,5 +103,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(async python-mode true qt-pro-mode ccls switch-window rjsx-mode company-qml qml-mode cmake-project eldoc-cmake all-the-icons modern-cpp-font-lock treemacs-all-the-icons yasnippet-snippets which-key vterm-toggle use-package undo-tree treemacs-projectile treemacs-magit treemacs-icons-dired symon smex rainbow-delimiters popup-kill-ring org-pdftools org-bullets nov lsp-ui ivy-xref ivy-rich ir-black-theme iedit ido-vertical-mode flycheck-clang-tidy doom-themes doom-modeline dmenu dired-subtree diminish diff-hl dashboard darkokai-theme darkmine-theme darkburn-theme dark-souls dark-mint-theme dark-krystal-theme dap-mode dakrone-theme d-mode cyberpunk-theme cyberpunk-2019-theme counsel-projectile constant-theme company-quickhelp company-box color-theme-sanityinc-tomorrow cmake-mode clang-format+ beacon badger-theme alect-themes afternoon-theme abyss-theme))
+   '(auctex-latexmk async python-mode true qt-pro-mode ccls switch-window rjsx-mode company-qml qml-mode cmake-project eldoc-cmake all-the-icons modern-cpp-font-lock treemacs-all-the-icons yasnippet-snippets which-key vterm-toggle use-package undo-tree treemacs-projectile treemacs-magit treemacs-icons-dired symon smex rainbow-delimiters popup-kill-ring org-pdftools org-bullets nov lsp-ui ivy-xref ivy-rich ir-black-theme iedit ido-vertical-mode flycheck-clang-tidy doom-themes doom-modeline dmenu dired-subtree diminish diff-hl dashboard darkokai-theme darkmine-theme darkburn-theme dark-souls dark-mint-theme dark-krystal-theme dap-mode dakrone-theme d-mode cyberpunk-theme cyberpunk-2019-theme counsel-projectile constant-theme company-quickhelp company-box color-theme-sanityinc-tomorrow cmake-mode clang-format+ beacon badger-theme alect-themes afternoon-theme abyss-theme))
  '(warning-suppress-types '((comp))))
